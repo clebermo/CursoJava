4abd341bd878696edca95237bee2d40211dc1250
clebermo <clebermoliveira@gmail.com>
2022-12-26 11:55:12 -0300

    Primeiro exercício curso Java Básico

---
+++ CursoJavaBasico/Exercicios/Exercicio01/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        int c = 5;
+        int resultado = a*b*c;
+
+        System.out.println("O resultado ");
+    }
+}
---
+++ CursoJavaBasico/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        int resultado = a+b;
+        System.out.println("O resultado de a+b é:" + resultado);
+
+    }
+
+
+}
