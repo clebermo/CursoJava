987b5dd38e06f62f742fe246a4fecab3f474f1d6
clebermo <clebermoliveira@gmail.com>
2022-12-27 10:38:31 -0300

    Excercícios de soma e multiplicação

---
+++ JavaBasico/CursoJavaBasico/src/main/java/org/example/Multiplicar.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Multiplicar {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 5;
+        int resultado = a * b * c;
+
+        System.out.println("O resultado da multiplicação é:" + resultado);
+    }
+}
