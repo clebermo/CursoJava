f12bbf14e105fff16ec87baf9f4e709eaa45e583
clebermo <clebermoliveira@gmail.com>
2023-01-11 10:09:32 -0300

    Exemplos e exercícios com Função Vazia

---
+++ JavaBasico/CursoJavaBasico/src/main/java/org/example/ExemploFuncaoVazia.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ExemploFuncaoVazia {
+
+    static void desenhaSeparador(){
+
+        System.out.println("-+-+-+-+-+-+-+-+-+");
+    }
+
+    public static void main(String[] args){
+
+        desenhaSeparador();
+        System.out.println("Oi");
+        desenhaSeparador();
+        System.out.println("Fim");
+
+    }
+}
---
+++ JavaBasico/CursoJavaBasico/src/main/java/org/example/ExercicioFuncaoVazia.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class ExercicioFuncaoVazia {
+
+        static void imprimirBomDia(){
+
+            System.out.println("Bom dia!");
+
+        }
+
+        
+    public static void main(String[] args){
+
+        //faça um programa que imprima "bom dia" dez vezes na tela
+        //usando uma função
+
+        for (int i = 0; i < 10; i++){
+
+            imprimirBomDia();
+        }
+
+    }
+}
